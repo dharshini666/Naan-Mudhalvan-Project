e93d51bd659351c9ba9a4fad05eab47a2e9e4b54
dharshini666 <dharshinidhas25@gmail.com>
2025-05-17 15:47:16 +0530

    Initial commit

---
+++ project.py
@@ -0,0 +1,37 @@
+import cv2
+
+def detect_underwater_objects(source):
+    cap = cv2.VideoCapture(source)
+    backSub = cv2.createBackgroundSubtractorMOG2(history=500, varThreshold=50, detectShadows=False)
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+
+        frame = cv2.resize(frame, (640, 480))
+
+
+        fg_mask = backSub.apply(frame)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+        fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_OPEN, kernel)
+
+        contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for cnt in contours:
+            if cv2.contourArea(cnt) > 500: 
+                x, y, w, h = cv2.boundingRect(cnt)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(frame, "Fish", (x, y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
+
+        cv2.imshow("Underwater Detection", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+detect_underwater_objects("nnnn.mp4")
